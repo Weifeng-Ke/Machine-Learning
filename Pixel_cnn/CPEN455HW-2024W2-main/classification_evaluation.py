bd0b83c7e1fa3be7f065911d9cf7ab469c6f40c3
Weifeng-Ke <kenweifeng@gmail.com>
2025-04-14 13:55:23 -0700

    modify path

--- Pixel_cnn/CPEN455HW-2024W2-main/classification_evaluation.py
+++ Pixel_cnn/CPEN455HW-2024W2-main/classification_evaluation.py
@@ -88,7 +88,7 @@ if __name__ == '__main__':
     model = model.to(device)
     #Attention: the path of the model is fixed to './models/conditional_pixelcnn.pth'
     #You should save your model to this path
-    model_path = os.path.join(os.path.dirname(__file__), 'models/conditional_pixelcnn.pth')
+    model_path = os.path.join(os.path.dirname(__file__), 'models/conditional_pixelcnn1.pth')
     if os.path.exists(model_path):
         model.load_state_dict(torch.load(model_path))
         print('model parameters loaded')
