9ea2a915f1cad4db251ebf36a13f31a34e265173
Weifeng-Ke <kenweifeng@gmail.com>
2025-04-15 16:29:35 -0700

    final double check

--- Pixel_cnn/CPEN455HW-2024W2-main/model.py
+++ Pixel_cnn/CPEN455HW-2024W2-main/model.py
@@ -56,7 +56,7 @@ class PixelCNNLayer_down(nn.Module):
 
 
 class PixelCNN(nn.Module):
-    def __init__(self, nr_resnet=5, nr_filters=80, nr_logistic_mix=10,
+    def __init__(self, nr_resnet=54, nr_filters=120, nr_logistic_mix=10,
                     resnet_nonlinearity='concat_elu', input_channels=3,embedding_dim=32):
         super(PixelCNN, self).__init__()
         if resnet_nonlinearity == 'concat_elu' :
