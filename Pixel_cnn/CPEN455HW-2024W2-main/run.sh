e00e8ba1cacb34ebad210b1a8d4361d40fbdfdcc
Weifeng-Ke <kenweifeng@gmail.com>
2025-04-15 19:55:41 -0700

    hell marry

--- Pixel_cnn/CPEN455HW-2024W2-main/run.sh
+++ Pixel_cnn/CPEN455HW-2024W2-main/run.sh
@@ -4,8 +4,8 @@ python pcnn_train.py \
 --sampling_interval 25 \
 --save_interval 25 \
 --dataset cpen455 \
---nr_resnet 4 \
---nr_filters 120 \
+--nr_resnet 3 \
+--nr_filters 100 \
 --nr_logistic_mix 10 \
 --lr_decay 0.999995 \
 --max_epochs 300 \
